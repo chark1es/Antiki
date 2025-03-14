f24daa2f4262053787155f71a4e79e8673c416e4
chark1es <charles15851@gmail.com>
2025-03-14 03:48:09 -0700

    fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  integrations: [react()],
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
